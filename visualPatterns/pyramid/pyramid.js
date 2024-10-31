c8716b1dc53052289e208657b1e03f48f28892ac
Rahul Pillai <r.rahul17498@gmail.com>
2024-10-30 22:05:06 -0400

    Add pyramid and inverted pyramid with N rows

---
+++ visualPatterns/pyramid/pyramid.js
@@ -0,0 +1,31 @@
+const cellPattern = "*";
+const emptyPattern = " ";
+
+const createPyramidWithNRows = (numOfRows) => {
+   const totalSpace = numOfRows + numOfRows - 1;
+   const row = new Array(totalSpace).fill(emptyPattern);
+   const mid = Math.ceil(totalSpace / 2) - 1;
+   console.log("mid: ", mid);
+   for (let i = 0; i < numOfRows; i++) {
+       row[mid + i] = cellPattern;
+       row[mid - i] = cellPattern;
+       console.log(row.join(""));
+   }
+};
+
+
+const createInvertedPyramidOfNRows = (numOfRows) => {
+   const totalSpace = numOfRows + numOfRows - 1;
+   const row = new Array(totalSpace).fill(cellPattern);
+   const mid = Math.ceil(totalSpace / 2) - 1;
+
+   for (let i = 0; i <= mid; i++) {
+      console.log(row.join(""));
+      row[i] = emptyPattern;
+      row[totalSpace - 1 - i] = emptyPattern; 
+   }
+};
+
+createPyramidWithNRows(5);
+
+createInvertedPyramidOfNRows(5);
